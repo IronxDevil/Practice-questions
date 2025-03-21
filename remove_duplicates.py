80c192e850b4aa123a9282320cd2267e2bd06713
IronxDevil <ironxdevil007@gmail.com>
2025-03-21 10:02:06 +0530

    first commit

--- remove_duplicates.py
+++ remove_duplicates.py
@@ -1,8 +1,9 @@
 arr = [1,1,2,2,3,3,3,4,5,5,6,7,7,7]
-j = 0
-for i in range(0, len(arr)):
-    if j != arr[i]:
+j = 1
+for i in range(1, len(arr)):
+    if arr[j] != arr[i - 1]:
         arr[j] = arr[i]
-        j += 1
+        
 arr = arr[: -(len(arr) - j)]
 print(arr)
+print(j)
